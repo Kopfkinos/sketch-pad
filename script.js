b16c82bedaf5f43a15b172a70bc53d7ed7e0caa2
IgelIntelligenz <connor_v_d_murphy@hotmail.com>
2025-07-17 22:57:19 +0100

    added logic to change colour of hovered over div, and added a reset and pixel density buttons

--- script.js
+++ script.js
@@ -1,11 +1,52 @@
 const container = document.querySelector("#container")
+const initialSize = 16
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < initialSize; i++) {
   const div = document.createElement("div")
   div.classList.add("row")
-  for (let j = 0; j < 16; j++) {
+  for (let j = 0; j < initialSize; j++) {
     const innerDiv = document.createElement("div")
     div.appendChild(innerDiv)
   }
   container.appendChild(div)
 }
+
+container.addEventListener("mouseover", (e) => {
+  let target = e.target
+  if (target.tagName === "DIV") {
+    target.classList.add("filled-in")
+  }
+})
+
+const resetBtn = document.querySelector(".reset")
+
+resetBtn.addEventListener("click", () => {
+  const pixels = container.querySelectorAll("div")
+  pixels.forEach((pixel) => {
+    pixel.classList.remove("filled-in")
+  })
+})
+
+const sizeBtn = document.querySelector(".size")
+
+sizeBtn.addEventListener("click", () => {
+  let inputSize = prompt("Input grid size")
+  while (inputSize > 100 || inputSize < 10 || !inputSize) {
+    inputSize = prompt("Input grid size (min 10, max 100)")
+  }
+
+  const existingGrid = container.querySelectorAll("div")
+  existingGrid.forEach((pixel) => {
+    pixel.remove()
+  })
+
+  for (let i = 0; i < inputSize; i++) {
+    const div = document.createElement("div")
+    div.classList.add("row")
+    for (let j = 0; j < inputSize; j++) {
+      const innerDiv = document.createElement("div")
+      div.appendChild(innerDiv)
+    }
+    container.appendChild(div)
+  }
+})
